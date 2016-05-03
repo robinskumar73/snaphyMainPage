9caea997566e14c991c7884a24c20175428d0b65
Robins Gupta <robinskumar73@gmail.com>
2016-05-03 13:54:24 -0400

    Adding snaphy cover page

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name comingSoonWebsiteApp
+ * @description
+ * # comingSoonWebsiteApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mainpage.snaphy', [
+    /*'ngAnimate',
+    'ngCookies',*/
+    'ngResource',
+    /*'ngRoute',*/
+    'ngSanitize',
+    'ui.router',
+    /*'ngTouch'*/
+    'angular-loading-bar'
+  ])
+
---
+++ scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name comingSoonWebsiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the comingSoonWebsiteApp
+ */
+angular.module('mainpage.snaphy')
+  .controller('MainCtrl', function () {
+
+  });
